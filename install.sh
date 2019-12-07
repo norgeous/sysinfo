8ae5cb5378f3b1ff41aaf2cb4445404032a19df5
im-dharte <daniel.harte@immediate.co.uk>
2019-12-07 22:06:09 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 [[ $EUID -ne 0 ]] && echo "You must be running as user root." && exit 1
 
+echo "installing sysstat and dnsutils..."
 apt install -y sysstat dnsutils
 
-# remove old install
+echo "remove old sysinfo install..."
 rm -r "/opt/sysinfo"
 
-# make install dir
+echo "make install dir..."
 mkdir -p "/opt/sysinfo"
 
-# download and install sysinfo script
+echo "download and install sysinfo script"
 curl https://raw.githubusercontent.com/norgeous/sysinfo/master/bin/sysinfo.sh --output "/opt/sysinfo/sysinfo.sh"
 chmod a+x "/opt/sysinfo/sysinfo.sh"
 [ ! -L "/usr/bin/sysinfo" ] && ln -s "/opt/sysinfo.sh" "/usr/bin/sysinfo"
 
-# download and install piv script
+echo "download and install piv script"
 curl https://raw.githubusercontent.com/norgeous/sysinfo/master/bin/piv.sh --output "/opt/sysinfo/piv.sh"
 chmod a+x "/opt/sysinfo/piv.sh"
 [ ! -L "/usr/bin/piv" ] && ln -s "/opt/sysinfo/piv.sh" "/usr/bin/piv"
