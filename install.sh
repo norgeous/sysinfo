ceec076117f380921d38ecf4acbfa981da6b2df1
im-dharte <daniel.harte@immediate.co.uk>
2019-12-07 21:47:57 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,18 +4,18 @@
 apt install -y sysstat dnsutils
 
 # remove old install
-rm -r "/usr/local/bin/sysinfo"
+rm -r "/opt/sysinfo"
 
 # make install dir
-mkdir -p "/usr/local/bin/sysinfo"
+mkdir -p "/opt/sysinfo"
 
 # download and install sysinfo script
-curl https://raw.githubusercontent.com/norgeous/sysinfo/master/bin/sysinfo.sh --output "/usr/local/bin/sysinfo"
-chmod a+x /usr/local/bin/sysinfo/sysinfo.sh
-[ ! -L "/usr/bin/sysinfo" ] && ln -s /opt/sysinfo.sh /usr/bin/sysinfo
-  
+curl https://raw.githubusercontent.com/norgeous/sysinfo/master/bin/sysinfo.sh --output "/opt/sysinfo/sysinfo.sh"
+chmod a+x "/opt/sysinfo/sysinfo.sh"
+[ ! -L "/usr/bin/sysinfo" ] && ln -s "/opt/sysinfo.sh" "/usr/bin/sysinfo"
+
 # download and install piv script
-curl https://raw.githubusercontent.com/norgeous/sysinfo/master/bin/piv.sh --output "/usr/local/bin/piv"
-chmod a+x /opt/piv.sh
-[ ! -L "/usr/bin/piv" ] && ln -s /opt/piv.sh /usr/bin/piv
+curl https://raw.githubusercontent.com/norgeous/sysinfo/master/bin/piv.sh --output "/opt/sysinfo/piv.sh"
+chmod a+x "/opt/sysinfo/piv.sh"
+[ ! -L "/usr/bin/piv" ] && ln -s "/opt/sysinfo/piv.sh" "/usr/bin/piv"
   
