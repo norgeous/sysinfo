ae6d97206424f63ab9d12d33e1af98ad675df5a3
im-dharte <daniel.harte@immediate.co.uk>
2019-12-07 22:14:25 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,13 +10,14 @@ rm -r "/opt/sysinfo"
 echo "make install dir..."
 mkdir -p "/opt/sysinfo"
 
-echo "download and install sysinfo script"
-curl https://raw.githubusercontent.com/norgeous/sysinfo/master/bin/sysinfo.sh --output "/opt/sysinfo/sysinfo.sh"
+echo "download and install sysinfo script..."
+curl --silent https://raw.githubusercontent.com/norgeous/sysinfo/master/bin/sysinfo.sh --output "/opt/sysinfo/sysinfo.sh"
 chmod a+x "/opt/sysinfo/sysinfo.sh"
 [ ! -L "/usr/bin/sysinfo" ] && ln -s "/opt/sysinfo.sh" "/usr/bin/sysinfo"
 
-echo "download and install piv script"
-curl https://raw.githubusercontent.com/norgeous/sysinfo/master/bin/piv.sh --output "/opt/sysinfo/piv.sh"
+echo "download and install piv script..."
+curl --silent  https://raw.githubusercontent.com/norgeous/sysinfo/master/bin/piv.sh --output "/opt/sysinfo/piv.sh"
 chmod a+x "/opt/sysinfo/piv.sh"
 [ ! -L "/usr/bin/piv" ] && ln -s "/opt/sysinfo/piv.sh" "/usr/bin/piv"
   
+echo "done"
